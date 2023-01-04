8c8f4e837525352e2b33086173ba5d31a067db2c
Davi Ricardo <davir35@gmail.com>
2023-01-03 21:36:15 -0300

    Adicionando construtores

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -4,6 +4,9 @@ import dto.Viagem;
 
 public class App {
     public static void main(String[] args) {
-        Viagem viagem = new Viagem();
+        Viagem viagem = new Viagem("Fortaleza");
+
+        System.out.println(viagem.destino);
+
     }
 }
